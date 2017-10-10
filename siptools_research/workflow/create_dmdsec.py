93f56d422b8add6f744c77d9ae7e4dc1292ad94f
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-10-10 14:43:59 +0300

    TPASPKT-23 Refactor create_dmdsec.py similarly as create_digiprov.py

--- siptools_research/workflow/create_dmdsec.py
+++ siptools_research/workflow/create_dmdsec.py
@@ -1,35 +1,27 @@
 """Luigi task that creates descriptive metadata."""
 
 import os
-import sys
-import traceback
-import datetime
-
+from datetime import datetime
 from luigi import Parameter, IntParameter, LocalTarget
-
-from siptools_research.workflow_x.move_sip import MoveSipToUser, FailureLog
-from siptools_research.luigi.target import TaskFileTarget, MongoDBTarget
-from siptools_research.utils.utils import touch_file
-
+from siptools_research.luigi.target import TaskFileTarget, MongoDBTarget, \
+    TaskLogTarget
 from siptools_research.luigi.task import WorkflowTask, WorkflowExternalTask
 from siptools_research.luigi.utils import file_age
-
+from siptools_research.utils.contextmanager import redirect_stdout
+from siptools_research.utils.utils import touch_file
+from siptools_research.workflow_x.move_sip import MoveSipToUser, FailureLog
 from siptools.scripts import import_description
 
 
 class CreateDescriptiveMetadata(WorkflowTask):
-    """Create mets dmdSec from EAD3 finding aid file.
-
+    """Create mets dmdSec from DataCite file.
     """
     workspace = Parameter()
     home_path = Parameter()
 
     def requires(self):
         """Return required tasks.
-
-        :returns: Files must have been transferred to workspace
         """
-
         return ReadyForThis(workspace=self.workspace, min_age=0)
 
 
@@ -41,7 +33,7 @@ class CreateDescriptiveMetadata(WorkflowTask):
         """
         Creates a METS dmdSec file from existing datacite.xml file. If
         unsuccessful writes an error message into mongoDB, updates the status
-        of the document and rejects the package. The rejected package is moved
+        of the document, and rejects the package. The rejected package is moved
         to the users home/rejected directory.
 
         :returns: None
@@ -49,7 +41,7 @@ class CreateDescriptiveMetadata(WorkflowTask):
         """
 
         sip_creation_path = os.path.join(self.workspace, 'sip-in-progress')
-        # TODO: Getting datacite.xml here from Metax is not implemented
+        # TODO: Getting datacite.xml from Metax is not implemented
         datacite_path = os.path.join(sip_creation_path, 'datacite.xml')
 
         document_id = os.path.basename(self.workspace)
@@ -58,40 +50,28 @@ class CreateDescriptiveMetadata(WorkflowTask):
         mongo_status = MongoDBTarget(document_id, 'status')
         mongo_timestamp = MongoDBTarget(document_id, 'timestamp')
 
+        dmdsec_log = TaskLogTarget(self.workspace,
+                                   'create-descriptive-metadata.log')
+
         try:
-            dmdsec_log = os.path.join(self.workspace, 'logs',
-                                      'task-create-descriptive-metadata.log')
-            save_stdout = sys.stdout
-            log = open(dmdsec_log, 'w')
-            sys.stdout = log
+            with dmdsec_log.open('w') as log:
+                with redirect_stdout(log):
+                    import_description.main([datacite_path,
+                                             '--workspace', sip_creation_path])
 
-            import_description.main([datacite_path,
-                                     '--workspace', sip_creation_path])
-
-            sys.stdout = save_stdout
-            log.close()
-
-            task_result = {
-                'timestamp': datetime.datetime.utcnow().isoformat(),
-                'result': 'success',
-                'messages': ("DataCite metadata wrapped into METS descriptive "
-                             "metadata section.")
-            }
-            mongo_task.write(task_result)
+            task_result = 'success'
+            task_messages = "DataCite metadata wrapped into METS descriptive "\
+                            "metadata section."
 
             # task output
             touch_file(TaskFileTarget(self.workspace,
                                       'create-descriptive-metadata'))
 
-        except Exception as ex:
-            task_result = {
-                'timestamp': datetime.datetime.utcnow().isoformat(),
-                'result': 'failure',
-                'messages': traceback.format_exc()
-            }
+        except Exception as exc:
+            task_result = 'failure'
+            task_messages = exc.message
+
             mongo_status.write('rejected')
-            mongo_timestamp.write(datetime.datetime.utcnow().isoformat())
-            mongo_task.write(task_result)
 
             failed_log = FailureLog(self.workspace).output()
             with failed_log.open('w') as outfile:
@@ -99,7 +79,25 @@ class CreateDescriptiveMetadata(WorkflowTask):
 
             yield MoveSipToUser(
                 workspace=self.workspace,
-                home_path=self.home_path)
+                home_path=self.home_path
+            )
+
+        finally:
+            if not task_result:
+                task_result = 'failure'
+                task_messages = "Creation of provenance metadata "\
+                                "failed due to unknown error."
+
+            mongo_task.write(task_result)
+            mongo_timestamp.write(datetime.utcnow().isoformat())
+            mongo_task.write(
+                {
+                    'timestamp': datetime.utcnow().isoformat(),
+                    'result': task_result,
+                    'messages': task_messages
+                }
+            )
+
 
 
 class ReadyForThis(WorkflowExternalTask):
