850aabdb74f940a7cc7ef485230d0ef2f508f834
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-10-10 18:06:51 +0300

    TPASPKT-27 Use fake-http-server-fixture in Metax tests

--- tests/utils/metax_test.py
+++ tests/utils/metax_test.py
@@ -1,24 +1,10 @@
 """Tests for `siptools_research.utils.metax` module"""
-import os
-import httpretty
 from siptools_research.utils.metax import Metax
 
 DATASET_PATH = "tests/data/metax_datasets/"
 
-@httpretty.activate
-def test_get_dataset():
+def test_get_dataset(testmetax):
     """Test get_dataset function"""
-    data_file_name = "provenance_data.json"
-    with open(os.path.join(DATASET_PATH, data_file_name)) as data_file:
-        data = data_file.read()
-
-    httpretty.register_uri(httpretty.GET,
-                           "https://metax-test.csc.fi/rest/v1/datasets/1",
-                           body=data,
-                           status=200,
-                           content_type='application/json'
-                          )
-
     dataset = Metax().get_data('datasets', "1")
     print dataset
     print type(dataset)
