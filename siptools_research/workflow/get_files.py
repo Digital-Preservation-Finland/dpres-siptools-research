16112ca5fc415768aabae6832e5db828c8699374
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-10-16 16:23:27 +0300

    TPASPKT-5 Get files from Ida

--- siptools_research/workflow/get_files.py
+++ siptools_research/workflow/get_files.py
@@ -3,6 +3,7 @@
 import os
 from luigi import Parameter
 from siptools_research.utils import ida
+from siptools_research.utils import metax
 from siptools_research.utils import database
 from siptools_research.luigi.target import MongoTaskResultTarget
 from siptools_research.luigi.task import WorkflowTask
@@ -18,24 +19,41 @@ class GetFiles(WorkflowTask):
     workspace = Parameter()
 
     def requires(self):
-        """Requires create dmdSec file task"""
+        """Required task: CreateWorkspace"""
         return CreateWorkspace(workspace=self.workspace,
                                dataset_id=self.dataset_id)
 
     def output(self):
+        """Adds report to mongodb when task is succesfully completed."""
         return MongoTaskResultTarget(self.document_id, self.task_name)
 
     def run(self):
-        """Gets files from Ida.
+        """Gets files from Ida. Adds result report to mongodb.
 
         :returns: None
         """
 
-        # TODO: Hard coded file id and filepath for testing
-        os.mkdir(os.path.join(self.workspace, 'files'))
-        filepath = os.path.join(self.workspace, 'files', 'file_1')
-        ida.download_file('pid:urn:1', filepath)
+        # Find file identifiers from Metax dataset metadata.
+        metax_client = metax.Metax()
+        dataset_metadata = metax_client.get_data('datasets',
+                                                 str(self.dataset_id))
+        for file_section in dataset_metadata['research_dataset']['files']:
+            file_id = file_section['identifier']
 
+            # Get file name and path from Metax file metadata
+            file_metadata = metax_client.get_data('files', file_id)
+            filename = file_metadata['file_name']
+            file_path = os.path.join(self.workspace,
+                                     'files',
+                                     file_metadata['file_path'].strip('/'))
+
+            # Download file from Ida to 'files' directory in workspace
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+            ida.download_file(file_id,
+                              os.path.join(file_path, filename))
+
+        # Add task report to mongodb
         database.add_event(self.document_id,
                            self.task_name,
                            'success',
