896c1e257a2fee9e1ae3cdf06e962ca29ada42ab
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-10-18 15:09:16 +0300

    Use database module to log task status

--- siptools_research/utils/database.py
+++ siptools_research/utils/database.py
@@ -7,11 +7,14 @@ HOST = 'localhost'
 DB = 'siptools-research'
 COLLECTION = 'workflow'
 
-# Initializing mongo client as global variable should allow using the same
-# client (same connection) across all modules by importing the MONGO_CLIENT
-# variable.
-MONGO_CLIENT = pymongo.MongoClient(host=HOST)
-MONGO_COLLECTION = MONGO_CLIENT[DB][COLLECTION]
+# TODO: Initializing mongo client as global variable should allow using the
+# same client (same connection) across all modules by importing the
+# MONGO_CLIENT variable. For example:
+#   MONGO_CLIENT = pymongo.MongoClient(host=HOST)
+#   MONGO_COLLECTION = MONGO_CLIENT[DB][COLLECTION]
+# However, this will cause some trouble in unit tests, where mongomock is used
+# as replacement for real mongodb. Therefore new mongo_client is created in
+# each function in this module.
 
 def timestamp():
     """Return time now."""
@@ -28,12 +31,31 @@ def add_event(document_id, taskname, result, messages):
     :returns: None
     """
 
-    MONGO_COLLECTION.update_one(
+    mongo_client = pymongo.MongoClient(host=HOST)
+    mongo_collection = mongo_client[DB][COLLECTION]
+
+    mongo_collection.update_one(
         {'_id': document_id},
         {'$set':{'workflow_tasks.' + taskname:{'timestamp': timestamp(),
                                                'messages': messages,
                                                'result': result}}
         },
+        upsert=True
+    )
+
+
+def set_status(document_id, status):
+    """Add information of workflow task to mongodb.
+
+    :document_id: Mongo document id
+    :taskname: Status string
+    """
+    mongo_client = pymongo.MongoClient(host=HOST)
+    mongo_collection = mongo_client[DB][COLLECTION]
+
+    mongo_collection.update_one(
+        {'_id': document_id},
+        {'$set':{'status': status}},
         upsert=True
     )
 
@@ -45,7 +67,10 @@ def add_dataset(document_id, dataset_id):
     :dataset_id: Dataset identifier
     :returns: None
     """
-    MONGO_COLLECTION.update_one(
+    mongo_client = pymongo.MongoClient(host=HOST)
+    mongo_collection = mongo_client[DB][COLLECTION]
+
+    mongo_collection.update_one(
         {'_id': document_id},
         {'$set':{'status': 'Request reveived',
                  'dataset': dataset_id}},
