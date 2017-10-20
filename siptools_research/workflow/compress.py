907a2f31a1f7e75abfe4bb4928f0a7207d9fa613
Pauliina Somerkoski <pauliina.somerkoski@csc.fi>
2017-10-20 11:00:50 +0300

    TPASPKT-12 initial version

--- siptools_research/workflow/compress.py
+++ siptools_research/workflow/compress.py
@@ -23,10 +23,6 @@ class CompressSIP(WorkflowTask):
 
     :path: workspace/created_sip
     """
-    workspace = Parameter()
-    sip_creation_path = Parameter()
-    sip_output_path = Parameter()
-    home_path = Parameter()
 
     def requires(self):
         """Requires signature file"""
@@ -36,12 +32,16 @@ class CompressSIP(WorkflowTask):
                         home_path=self.home_path)}
 
     def output(self):
-        """Outputs a task file"""
-        return TaskFileTarget(self.workspace, 'compress-sip')
+       """Returns task output. Task is ready when succesful event has been
+        added to worklow database.
 
+        :returns: MongoTaskResultTarget
+        """
+       return MongoTaskResultTarget(document_id=self.document_id,
+                                     taskname=self.task_name)
     def run(self):
         """Creates a tar archive file conatining mets.xml,
-        signature.sig, sahke2.xml and all content files.
+        signature.sig  and all content files.
         If unsuccessful writes an error message into mongoDB, updates
         the status of the document and rejects the package. The rejected
         package is moved to the users home/rejected directory.
@@ -49,57 +49,37 @@ class CompressSIP(WorkflowTask):
         :returns: None
 
         """
-        if not os.path.isdir(self.sip_output_path):
-            os.makedirs(self.sip_output_path)
-
-        document_id = os.path.basename(self.workspace)
-        mongo_task = MongoDBTarget(document_id, 'wf_tasks.compress-sip')
-        mongo_status = MongoDBTarget(document_id, 'status')
-        mongo_timestamp = MongoDBTarget(document_id, 'timestamp')
 
         sip_name = os.path.join(self.sip_output_path,
                                 (os.path.basename(self.workspace) + '.tar'))
 
-        try:
-            compress_log = os.path.join(self.workspace, 'logs',
-                                        'task-compress-sip.log')
-            save_stdout = sys.stdout
-            log = open(compress_log, 'w')
-            sys.stdout = log
+        compress_log = os.path.join(self.workspace,
+                                    'logs',
+                                    'task-compress-sip.log')
+         # Redirect stdout to logfile
+        with open(compress_log, 'w') as log:
+            with redirect_stdout(log):
+                try:
 
-            main(['--tar_filename', sip_name, self.sip_creation_path])
+                    main(['--tar_filename', sip_name, self.sip_creation_path])
+                    task_result = 'success'
+                    task_messages = "SIP archive file compressed"
 
-            sys.stdout = save_stdout
-            log.close()
 
-            task_result = {
-                'timestamp': datetime.datetime.utcnow().isoformat(),
-                'result': 'success',
-                'messages': ("Transfer files and metadata compressed into "
-                             "SIP archive file.")
-            }
-            mongo_task.write(task_result)
-            mongo_timestamp.write(datetime.datetime.utcnow().isoformat())
-            mongo_status.write('SIP created')
 
-            # task output
-            touch_file(TaskFileTarget(self.workspace, 'compress-sip'))
+                except Exception as ex:
+                   task_result = 'failure'
+                   task_messages = 'Could not compress SIP, '\
+                                    % exc.message
 
-        except Exception as ex:
-            task_result = {
-                'timestamp': datetime.datetime.utcnow().isoformat(),
-                'result': 'failure',
-                'messages': traceback.format_exc()
-            }
+                   database.set_status(self.document_id, 'rejected')
+                finally:
+                    if not task_result:
+                        task_result = 'failure'
+                        task_messages = "Compression of SIP "\
+                                        "failed due to unknown error."
 
-            mongo_status.write('rejected')
-            mongo_timestamp.write(datetime.datetime.utcnow().isoformat())
-            mongo_task.write(task_result)
-
-            failed_log = FailureLog(self.workspace).output()
-            with failed_log.open('w') as outfile:
-                outfile.write('Task compress-sip failed.')
-
-            yield MoveSipToUser(
-                workspace=self.workspace,
-                home_path=self.home_path)
+                    database.add_event(self.document_id,
+                                       self.task_name,
+                                       task_result,
+                                       task_messages)
--- siptools_research/workflow/send_sip.py
+++ siptools_research/workflow/send_sip.py
@@ -20,9 +20,6 @@ from siptools_research.workflow.compress import CompressSIP
 class SendSIPToDP(WorkflowTask):
     """Send SIP to DP.
     """
-    workspace = Parameter()
-    sip_creation_path = Parameter()
-    sip_output_path = Parameter()
     home_path = Parameter()
 
     def requires(self):
@@ -36,11 +33,14 @@ class SendSIPToDP(WorkflowTask):
                         home_path=self.home_path)}
 
     def output(self):
-        """Return output target for the task.
+        """Returns task output. Task is ready when succesful event has been
+         added to worklow database.
 
-        :returns: TaskFileTarget object
-        """
-        return TaskFileTarget(self.workspace, 'send-sip-to-dp')
+         :returns: MongoTaskResultTarget
+        """  
+        return MongoTaskResultTarget(document_id=self.document_id,
+                                     taskname=self.task_name)
+       
 
     def run(self):
         """Sends SIP file to DP service using sftp.
@@ -50,78 +50,37 @@ class SendSIPToDP(WorkflowTask):
 
         :returns: None
         """
-        document_id = os.path.basename(self.workspace)
-        mongo_task = MongoDBTarget(document_id,
-                                   'wf_tasks.send-sip-to-dp-service')
-        mongo_status = MongoDBTarget(document_id, 'status')
-        mongo_timestamp = MongoDBTarget(document_id, 'timestamp')
 
         sip_name = os.path.join(self.sip_output_path,
                                 (os.path.basename(self.workspace) + '.tar'))
 
         # Check how many times task has been run
-        count = 0
-        sent_log = os.path.join(self.workspace, 'logs',
-                                'task-send-sip-to-dp.log')
-        if os.path.isfile(sent_log):
-            log_file = open(sent_log, 'r')
-            first_line = log_file.readline()
-            count = int(first_line.split(' ', 1)[0])
-            log_file.close()
+        sent_log = os.path.join(self.workspace,
+                                  "logs",
+                                  'task-send-sip-to-dp.log')
 
-        # Run task if count is < 10
-        if count < 10:
-            try:
-                (outcome, err) = send_to_dp(sip_name, sent_log, count)
+        try:
+             with open(sent_log, 'w') as log:
+                 with redirect_stdout(log):
 
-                if outcome == 'success':
-                    task_result = {
-                        'timestamp': datetime.datetime.utcnow().isoformat(),
-                        'result': 'success',
-                        'messages': ("SIP sent to DP service for "
-                                     "ingest.")
-                    }
-                    mongo_task.write(task_result)
-                    mongo_timestamp.write(
-                        datetime.datetime.utcnow().isoformat())
-                    mongo_status.write('SIP sent to DP service')
+                   (outcome, err) = send_to_dp(sip_name, sent_log, count)
+                   if outcome == 'success':
+                      task_result = 'success'
+                      task_messages = "Send to DP successfully "
+                   else: 
+                      task_result = 'failure'
+                      task_messages =  err
 
-                    # task output
-                    touch_file(TaskFileTarget(self.workspace,
-                                              'send-sip-to-dp'))
+        finally:
+                if not task_result:
+                    task_result = 'failure'
+                    task_messages = "Sending SIP to dp "\
+                                "failed due to unknown error."
 
-                else:
-                    task_result = {
-                        'timestamp': datetime.datetime.utcnow().isoformat(),
-                        'result': 'failure',
-                        'messages': err
-                    }
-                    mongo_task.write(task_result)
-
-                    failed_log = FailureLog(self.workspace).output()
-                    with failed_log.open('w') as outfile:
-                        outfile.write('Failed to send SIP to DP.')
-
-            except:
-                task_result = {
-                    'timestamp': datetime.datetime.utcnow().isoformat(),
-                    'result': 'failure',
-                    'messages': traceback.format_exc()
-                }
-                mongo_task.write(task_result)
-
-                failed_log = FailureLog(self.workspace).output()
-                with failed_log.open('w') as outfile:
-                    outfile.write('Failed to send SIP to DP.')
-
-        # Complete task and set status to pending
-        elif count == 10:
-            mongo_status.write('pending')
-            mongo_timestamp.write(datetime.datetime.utcnow().isoformat())
-
-            # task output
-            touch_file(TaskFileTarget(self.workspace, 'send-sip-to-dp'))
-
+                database.add_event(self.document_id,
+                               self.task_name,
+                               task_result,
+                               task_messages)
 
 class SendSIPComplete(WorkflowExternalTask):
     """Task that completes after SIP has been sent to DP.
@@ -137,9 +96,9 @@ def send_to_dp(sip, sent_log, count):
     """Sends SIP to DP service using sftp.
     """
     count += 1
-    identity = '~/.ssh/id_rsa_pastesti_Kansallisarkisto'
-    host = '86.50.168.61'
-    username = 'kansallisarkisto'
+    identity = '~/.ssh/id_rsa_tpas_pouta'
+    host = '86.50.168.218'
+    username = 'tpas'
 
     connection = '%s@%s:transfer' % (username, host)
     identityfile = '-oIdentityFile=%s' % identity
@@ -148,11 +107,6 @@ def send_to_dp(sip, sent_log, count):
     command = 'put %s' % sip
     cmd = 'sftp %s %s' % (identityfile, connection)
 
-    # Write stdout to log
-    save_stdout = sys.stdout
-    log = open(sent_log, 'w')
-    sys.stdout = log
-    print count
 
     ssh = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE, shell=True)
@@ -163,12 +117,7 @@ def send_to_dp(sip, sent_log, count):
 
     if returncode == 0:
         print 'Transfer completed successfully.'
-        outcome = 'success'
-    else:
-        print err
+        outcome = 'success' 
 
-    # Close log
-    sys.stdout = save_stdout
-    log.close()
 
     return outcome, err
