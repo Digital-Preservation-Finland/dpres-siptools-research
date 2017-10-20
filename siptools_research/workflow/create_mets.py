9e0b9832d780f0b1866e45db5e76fa911899ce34
Pauliina Somerkoski <pauliina.somerkoski@csc.fi>
2017-10-20 10:59:25 +0300

    TPASPKT-17 cleaned up

--- siptools_research/workflow/create_mets.py
+++ siptools_research/workflow/create_mets.py
@@ -11,10 +11,11 @@ from luigi import Parameter
 
 from siptools_research.workflow_x.move_sip import MoveSipToUser, FailureLog
 from siptools_research.luigi.target import TaskFileTarget, MongoDBTarget, TaskLogTarget
+from siptools_research.luigi.target import MongoTaskResultTarget
 from siptools_research.utils.utils import  touch_file
 from siptools_research.utils.metax import Metax
 from siptools_research.utils.contextmanager import redirect_stdout
-
+from siptools_research.utils import database
 from siptools_research.luigi.task import WorkflowTask
 
 from siptools_research.workflow.create_structmap import CreateStructMap
@@ -35,7 +36,7 @@ class CreateMets(WorkflowTask):
 
     def output(self):
         """Outputs a task file"""
-        return TaskFileTarget(self.workspace, 'create-mets')
+        return MongoTaskResultTarget(self.document_id, self.task_name)
 
     def run(self):
         """Compiles all metadata files into METS document.
@@ -46,9 +47,6 @@ class CreateMets(WorkflowTask):
 
         """
         document_id = os.path.basename(self.workspace)
-        mongo_task = MongoDBTarget(document_id, 'wf_tasks.create-mets')
-        mongo_status = MongoDBTarget(document_id, 'status')
-        mongo_timestamp = MongoDBTarget(document_id, 'timestamp')
         task_result = None
         task_messages = None
         mets_log = TaskLogTarget(self.workspace,
@@ -63,7 +61,6 @@ class CreateMets(WorkflowTask):
                           'tpas', 'tpas', '--clean']) #, --contract_id, contract_id ])
                     task_result = 'success'
                     task_messages = "Mets dodument created."
-                    mongo_task.write(task_result)
 
                    # task output
                     touch_file(TaskFileTarget(self.workspace, 'create-mets'))
@@ -74,29 +71,16 @@ class CreateMets(WorkflowTask):
                                     'element "%s" not found from metadata.'\
                                     % ex.message
 
-                    failed_log = FailureLog(self.workspace).output()
-                    with failed_log.open('w') as outfile:
-                       outfile.write('Task compile mets document failed.')
 
-                    mongo_status.write('rejected')
                 except Exception as e:
                    print "except %s " % e
                 finally:
-                    print "finally"
+           
                     if not task_result:
                         task_result = 'failure'
                         task_messages = "Compilation of mets document "\
                                         "failed due to unknown error."
-
-                    mongo_timestamp.write(
-                        datetime.utcnow().isoformat()
-                   )
-
-                    mongo_task.write(
-                    {
-                     'timestamp': datetime.utcnow().isoformat(),
-                     'result': task_result,
-                     'messages': task_messages
-                    }
-                    )
-
+                    database.add_event(self.document_id,
+                                       self.task_name,
+                                       task_result,
+                                       task_messages)
