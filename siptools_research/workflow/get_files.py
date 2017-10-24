93693225a6183c352b1bbeec90fe9ab475a95dd5
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-10-24 17:33:15 +0300

    TPASPKT-5 Read ida file id from metax file metadata

--- siptools_research/workflow/get_files.py
+++ siptools_research/workflow/get_files.py
@@ -10,21 +10,30 @@ from siptools_research.workflow.create_workspace import CreateWorkspace
 
 
 class GetFiles(WorkflowTask):
-    """Create provenance information as PREMIS event and PREMIS agent
-    files in METS digiprov wrappers.
+    """A task that reads file metadata from Metax and downloads requred files
+    from Ida.
     """
 
     def requires(self):
-        """Required task: CreateWorkspace"""
+        """Returns list of required tasks. This task requires task:
+        CreateWorkspace.
+
+        :returns: CreateWorkspace task
+        """
         return CreateWorkspace(workspace=self.workspace,
                                dataset_id=self.dataset_id)
 
     def output(self):
-        """Adds report to mongodb when task is succesfully completed."""
+        """Returns output target. This task reports to mongodb when task is
+        succesfully completed.
+
+        :returns: MongoTaskResult
+        """
         return MongoTaskResultTarget(self.document_id, self.task_name)
 
     def run(self):
-        """Gets files from Ida. Adds result report to mongodb.
+        """Reads list of required files from Metax and downloads them from Ida.
+        Adds result report to mongodb.
 
         :returns: None
         """
@@ -34,7 +43,7 @@ class GetFiles(WorkflowTask):
         dataset_metadata = metax_client.get_data('datasets',
                                                  str(self.dataset_id))
         for file_section in dataset_metadata['research_dataset']['files']:
-            file_id = file_section['identifier']
+            metax_file_id = file_section['identifier']
 
             # The path where file should be written in SIP is stored in 'type'
             # field in datasets files section
@@ -45,13 +54,17 @@ class GetFiles(WorkflowTask):
             )
 
             # Name of the file comes from file metadata
-            file_metadata = metax_client.get_data('files', file_id)
+            file_metadata = metax_client.get_data('files', metax_file_id)
             filename = file_metadata['file_name']
 
-            # Download file from Ida to 'sip-in-progress' directory in workspace
+            # Ida file id comes from file metadata
+            ida_file_id = file_metadata['identifier']
+
+            # Download file from Ida to 'sip-in-progress' directory in
+            # workspace
             if not os.path.exists(file_path):
                 os.makedirs(file_path)
-            ida.download_file(file_id,
+            ida.download_file(ida_file_id,
                               os.path.join(file_path, filename))
 
         # Add task report to mongodb
