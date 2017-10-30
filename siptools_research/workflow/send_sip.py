19f9b05f8e46f81742f46e8f3700ea6e0fe1a351
Pauliina Somerkoski <pauliina.somerkoski@csc.fi>
2017-10-30 12:54:44 +0200

    TPASPKT-12

--- siptools_research/workflow/send_sip.py
+++ siptools_research/workflow/send_sip.py
@@ -8,13 +8,12 @@ import subprocess
 
 from luigi import Parameter
 
-from siptools_research.workflow_x.move_sip import FailureLog
-from siptools_research.luigi.target import TaskFileTarget, MongoDBTarget
-
+from siptools_research.luigi.target import MongoTaskResultTarget
 from siptools_research.luigi.task import WorkflowTask, WorkflowExternalTask
 
 from siptools_research.workflow.compress import CompressSIP
-from siptools_research.utils import database
+from siptools_research.utils import database, utils
+from siptools_research.utils.contextmanager import redirect_stdout
 
 class SendSIPToDP(WorkflowTask):
     """Send SIP to DP.
@@ -52,31 +51,33 @@ class SendSIPToDP(WorkflowTask):
         sip_name = os.path.join(self.sip_path,
                                 (os.path.basename(self.workspace) + '.tar'))
 
-        
         sent_log = os.path.join(self.workspace,
                                   "logs",
                                   'task-send-sip-to-dp.log')
-            
+        utils.makedirs_exist_ok(os.path.join(self.workspace, "logs"))   
         task_result = None
+        open(sent_log, 'a')
         try:
-             with open(sent_log, 'w+') as log:
-                 with redirect_stdout(log):
+            with open(sent_log, 'w+') as log:
+                with redirect_stdout(log):
+                    (outcome, err) = send_to_dp(sip_name, sent_log)
+                    if outcome == 'success':
+                       task_result = 'success'
+                       task_messages = "Send to DP successfully "
+                    else: 
+                       task_result = 'failure'
+                       task_messages =  err
 
-                   (outcome, err) = send_to_dp(sip_name, sent_log)
-                   if outcome == 'success':
-                      task_result = 'success'
-                      task_messages = "Send to DP successfully "
-                   else: 
-                      task_result = 'failure'
-                      task_messages =  err
-
+        except:
+            print "exception"
+            print  "Exception ", sys.exc_info()[0]
         finally:
-                if not task_result:
-                    task_result = 'failure'
-                    task_messages = "Sending SIP to dp "\
+            if not task_result:
+                 task_result = 'failure'
+                 task_messages = "Sending SIP to dp "\
                                 "failed due to unknown error."
 
-                database.add_event(self.document_id,
+            database.add_event(self.document_id,
                                self.task_name,
                                task_result,
                                task_messages)
@@ -94,6 +95,7 @@ class SendSIPComplete(WorkflowExternalTask):
 def send_to_dp(sip, sent_log):
     """Sends SIP to DP service using sftp.
     """
+    print "send-to-dp"
     identity = '~/.ssh/id_rsa_tpas_pouta'
     host = '86.50.168.218'
     username = 'tpas'
@@ -105,17 +107,14 @@ def send_to_dp(sip, sent_log):
     command = 'put %s' % sip
     cmd = 'sftp %s %s' % (identityfile, connection)
 
-
     ssh = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE, shell=True)
-
     out, err = ssh.communicate(command)
     returncode = ssh.returncode
-    print out
 
     if returncode == 0:
         print 'Transfer completed successfully.'
         outcome = 'success' 
-
+        print "return 0"
 
     return outcome, err
