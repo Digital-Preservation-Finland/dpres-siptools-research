7e3271ded8612b06e038525cb989ea4d96b7be2d
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-11-06 18:22:14 +0200

    TPASPKT-9 Pylint style check

--- tests/workflow/create_structmap_test.py
+++ tests/workflow/create_structmap_test.py
@@ -2,26 +2,9 @@
 
 import os
 import shutil
-import getpass
-from uuid import uuid4
-
-import pytest
-
 from tests.assertions import task_ok
-
 from siptools_research.luigi.target import mongo_settings
-
-from siptools_research.luigi.utils import UnknownReturnCode
 from siptools_research.workflow.create_structmap import CreateStructMap
-from siptools_research.workflow.create_techmd import CreateTechnicalMetadata
-from siptools_research.workflow.create_digiprov import CreateProvenanceInformation
-from siptools_research.workflow.create_dmdsec import CreateDescriptiveMetadata
-#from siptools_research.create_sip.decompress import DecompressTransfer
-#from siptools_research.create_sip.virus import ScanVirus
-#from siptools_research.transfer.transfer import MoveTransferToWorkspace
-
-from siptools_research.workflow import create_digiprov
-from siptools_research.workflow.create_dmdsec import CreateDescriptiveMetadata
 from siptools.scripts import import_object
 from siptools.scripts import import_description
 
@@ -31,11 +14,8 @@ def test_create_structmap_ok(testpath):
     """
     workspace = testpath
 
-     # Clean workspace and create "logs" directory in
+    # Clean workspace and create "logs" directory in
     # temporary directory
-    if os.path.exists(workspace):
-        shutil.rmtree(workspace)
-    os.makedirs(workspace)
     os.makedirs(os.path.join(workspace, 'logs'))
     # Copy sample datacite.xml to workspace directory
     dmdpath = os.path.join(workspace, 'datacite.xml')
@@ -44,17 +24,13 @@ def test_create_structmap_ok(testpath):
     # Create dmdsec
     import_description.main([dmdpath, '--workspace', workspace])
 
-     # Create provenance
+    # Create provenance
     testfilename = "aineisto"
-    testfilepath = os.path.join(workspace,  testfilename)
+    testfilepath = os.path.join(workspace, testfilename)
     with open(testfilepath, 'w') as testfile:
         testfile.write('1')
     assert os.path.isfile(testfilepath)
 
-    # create provenance
-    # task = create_digiprov.CreateProvenanceInformation(home_path=testpath,
-    #                                                   workspace=workspace)
-    
     # Create tech metadata
     test_data_folder = './tests/data/structured'
 
@@ -62,7 +38,7 @@ def test_create_structmap_ok(testpath):
 
     # Create structmap
     task = CreateStructMap(workspace=workspace, dataset_id='1')
-    
+
     task.run()
 
     assert task_ok(task)
@@ -81,12 +57,13 @@ def assert_mongodb_data_success(document_id):
     (mongo_client, mongo_db, mongo_col) = mongo_settings()
     mongodb = mongo_client[mongo_db]
     collection = mongodb[mongo_col]
-    mongodb_data = collection.find({"_id": document_id},
-            {"wf_tasks.create-structural-map-and-file-section.result": 1})
+    mongodb_data = collection.find(
+        {"_id": document_id},
+        {"wf_tasks.create-structural-map-and-file-section.result": 1})
 
     for item in mongodb_data:
-        assert item["wf_tasks"]["create-structural-map-and-file-section"]["result"] == "success"
+        assert item["wf_tasks"]["create-structural-map-and-file-section"]\
+            ["result"] == "success"
 
     # remove document from database
     collection.remove({"_id": document_id})
-
