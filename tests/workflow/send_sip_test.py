f1561bc6351e6dd827a71f7dc20ceeeb8753380a
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-11-08 19:17:33 +0200

    Remove unused imports and trailing whitespace

--- tests/workflow/send_sip_test.py
+++ tests/workflow/send_sip_test.py
@@ -1,17 +1,8 @@
-import shutil
-import getpass
-import grp
+"""Test ``siptools_research.workflow.send_sip`` module"""
 import os
-
-import pytest
-
 from tests.assertions import task_ok
-
-from siptools_research.luigi.target import mongo_settings
-from siptools.scripts import import_object
-from siptools.scripts import import_description, premis_event, compile_structmap
-from siptools_research.workflow.send_sip import SendSIPToDP
 from siptools.scripts.compress import main
+from siptools_research.workflow.send_sip import SendSIPToDP
 
 def test_send_sip(testpath, monkeypatch):
     """Test the workflow task SendSip module.
@@ -28,10 +19,10 @@ def test_send_sip(testpath, monkeypatch):
     sip_name = os.path.join(create_sip,
                                 (os.path.basename(workspace) + '.tar'))
     main(['--tar_filename', sip_name, create_sip])
- 
-    
+
+
     #test create mets task
-    task = SendSIPToDP(workspace=workspace, sip_path='tests/data/testsip', dataset_id='1') 
+    task = SendSIPToDP(workspace=workspace, sip_path='tests/data/testsip', dataset_id='1')
     #SendSIPToDP(workspace=workspace, sip_output_path='tests/data/testsip', sip_creation_path=create_sip, home_path=workspace, dataset_id='1')
     task.run()
     assert task_ok(task)
