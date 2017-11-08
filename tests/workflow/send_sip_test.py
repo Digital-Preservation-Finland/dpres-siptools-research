dac1974db7722a2cc24a63710142f7793edcf76b
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-11-08 19:21:49 +0200

    Pylint style check

--- tests/workflow/send_sip_test.py
+++ tests/workflow/send_sip_test.py
@@ -14,27 +14,23 @@ def test_send_sip(testpath, monkeypatch):
                         'tests/data/pas_ssh_key')
 
     workspace = testpath
-    create_sip = 'tests/data/testsip' #os.path.join(workspace, "sip-in-progress")
+    create_sip = 'tests/data/testsip'
     #tar testsip
     sip_name = os.path.join(create_sip,
-                                (os.path.basename(workspace) + '.tar'))
+                            (os.path.basename(workspace) + '.tar'))
     main(['--tar_filename', sip_name, create_sip])
 
-
-    #test create mets task
-    task = SendSIPToDP(workspace=workspace, sip_path='tests/data/testsip', dataset_id='1')
-    #SendSIPToDP(workspace=workspace, sip_output_path='tests/data/testsip', sip_creation_path=create_sip, home_path=workspace, dataset_id='1')
+    # Init and run task
+    task = SendSIPToDP(workspace=workspace,
+                       sip_path=create_sip,
+                       dataset_id='1')
     task.run()
     assert task_ok(task)
     assert task.complete()
     assert_mongodb_data_success(workspace)
 
 
-
-
 def assert_mongodb_data_success(document_id):
     """Asserts that the task has written a successful outcome to
     MongoDB.
     """
-
-
