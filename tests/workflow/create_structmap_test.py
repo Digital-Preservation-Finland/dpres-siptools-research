d208bba1def84daf0c323c08a941cfe5bdb009b1
Pauliina Somerkoski <pauliina.somerkoski@csc.fi>
2017-12-13 15:24:49 +0200

    TPAPKT-43 added check for logical structmap

--- tests/workflow/create_structmap_test.py
+++ tests/workflow/create_structmap_test.py
@@ -39,7 +39,6 @@ def test_create_structmap_ok(testpath, testmongoclient):
     import_object.main([test_data_folder, '--workspace', sip_creation_path])
 
     #logical structmap
-    #logical_struct = {'Source material': ['/some/path/file_name_1']}
     logical_struct = {'Access and use rights files': ['tests/data/structured/Access and use rights files/access_file.txt'],'Documentation files': ['tests/data/structured/Documentation files/readme.txt', 'tests/data/structured/Documentation files/Configuration files/properties.txt', 'tests/data/structured/Documentation files/Method files/method_putkisto.txt', 'tests/data/structured/Documentation files/Notebook/notes.txt', 'tests/data/structured/Documentation files/Other files/this.txt'],'Machine-readable metadata': ['tests/data/structured/Machine-readable metadata/metadata.txt'],'Publication files': ['tests/data/structured/Publication files/publication.txt'], 'Software files': ['tests/data/structured/Software files/koodi.java']}
     with open(os.path.join(workspace,
                                  'sip-in-progress','logical_struct'), 'w') as new_file:
@@ -54,6 +53,11 @@ def test_create_structmap_ok(testpath, testmongoclient):
     assert os.path.isfile(os.path.join(sip_creation_path, 'filesec.xml'))
     assert os.path.isfile(os.path.join(sip_creation_path, 'structmap.xml'))
 
+    with open(os.path.join(sip_creation_path,
+                           'structmap.xml'))\
+             as open_file:
+        assert 'Fairdata-logical' in open_file.read()
+
     assert_mongodb_data_success(workspace)
 
 def test_create_structmap_get_files_ok(testpath, testmongoclient):
@@ -61,17 +65,17 @@ def test_create_structmap_get_files_ok(testpath, testmongoclient):
 
     workspace = testpath
     sip_creation_path = os.path.join(workspace, "sip-in-progress")
-    
+
      # Clean workspace and create "logs" directory in temporary directory
     os.makedirs(os.path.join(workspace, 'logs'))
     os.makedirs(os.path.join(workspace, 'sip-in-progress'))
       # Copy sample datacite.xml to workspace directory
     dmdpath = os.path.join(workspace, 'datacite.xml')
     shutil.copy('tests/data/datacite_sample.xml', dmdpath)
-    
+
      # Create dmdsec
     import_description.main([dmdpath, '--workspace', sip_creation_path])
-     
+
      # Create provenance
     testfilename = "aineisto"
     testfilepath = os.path.join(workspace, testfilename)
@@ -81,7 +85,7 @@ def test_create_structmap_get_files_ok(testpath, testmongoclient):
 
    # get_files.GetFiles(workspace=testpath,
    #                           dataset_id="2") 
-    #logical_struct = {'Source material': ['/some/path/file_name_1']}
+
     logical_struct = {'Source material': ['tests/data/text-file.txt']}
     with open(os.path.join(workspace,
                            'sip-in-progress','logical_struct'), 'w') as new_file:
