3b0af0fa118c7a4e38cd1808b4fd76ef7ac2e89a
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-12-15 12:45:38 +0200

    TPASPKT-51 Add workflow integration test

---
+++ tests/integration_tests/workflow_test.py
@@ -0,0 +1,57 @@
+"""Tests running parts of workflow"""
+
+import pytest
+import luigi.cmdline
+import pymongo
+from siptools_research.config import Configuration
+
+
+def run_luigi_task(module, task, workspace):
+    """Run luigi as it would be run from commandline"""
+    with pytest.raises(SystemExit):
+        luigi.cmdline.luigi_run(
+            ('--module',
+             module,
+             task,
+             '--workspace', workspace,
+             '--dataset-id', 'workflow_test_dataset_1',
+             '--config', 'tests/data/siptools_research.conf',
+             '--local-scheduler',
+             '--no-lock')
+        )
+
+
+# Run every task as it would be run from commandline
+@pytest.mark.parametrize(
+    "module,task", [
+        # ('cleanup', 'CleanupWorkspace'),
+        # ('compress', 'CompressSIP'),
+        ('create_digiprov', 'CreateProvenanceInformation'),
+        ('create_dmdsec', 'CreateDescriptiveMetadata'),
+        # ('create_mets', 'CreateMets'),
+        # ('create_structmap', 'CreateStructMap'),
+        ('create_techmd', 'CreateTechnicalMetadata'),
+        ('create_workspace', 'CreateWorkspace'),
+        # ('get_files', 'GetFiles'),
+        # ('report_preservation_status', 'ReportPreservationStatus'),
+        # ('send_sip', 'SendSIPToDP'),
+        # ('sign', 'SignSIP'),
+    ]
+)
+def test_workflow(testpath, testmetax, testmongoclient, module, task):
+    """Run a task (and all tasks it requires) and check that check that report
+    of successfull task is added to mongodb.
+    """
+    workspace = testpath
+    run_luigi_task('siptools_research.workflow.' + module,
+                   task,
+                   workspace)
+
+    conf = Configuration('tests/data/siptools_research.conf')
+    mongoclient = pymongo.MongoClient(host=conf.get('mongodb_host'))
+    collection = mongoclient[conf.get('mongodb_database')]\
+        [conf.get('mongodb_collection')]
+    document = collection.find_one()
+    # Check 'result' field
+    assert document['workflow_tasks'][task]['result'] ==\
+        'success'
