f63718b002d1c2a36c3fbff9415d4c3184088a45
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-12-27 14:36:27 +0200

    TPASPKT-67 More escriptive variable names and cleaner code

--- siptools_research/workflow/create_structmap.py
+++ siptools_research/workflow/create_structmap.py
@@ -59,47 +59,43 @@ class CreateStructMap(WorkflowTask):
         # Redirect stdout to logfile
         with self.output().open('w') as log:
             with redirect_stdout(log):
-                #create structmap
+
+                # create physical structmap
                 compile_structmap.main(['--workspace',
                                         self.sip_creation_path])
-                # pylint: disable=no-member
-                filesec_xml = ET.parse(os.path.join(self.sip_creation_path,
-                                                    'filesec.xml'))
-
-                # Get filecategories for each file
-                categories = self.find_file_categories()
-                # create logical structmap
-                structmap = mets.structmap(type_attr='Fairdata-logical')
 
                 # Read the previously (in compile_structmap) generated
                 # physical structmap from file
+                # pylint: disable=no-member
                 struct = ET.parse(os.path.join(self.sip_creation_path,
                                                'structmap.xml'))
-                # get dmd id
-                root = struct.getroot()
-                old_struct = root[0][0]
+                # Get dmdsec id
+                old_struct = struct.getroot()[0][0]
                 dmdsec_id = [old_struct.attrib['DMDID']]
 
+                # Init logical structmap
+                logical_structmap = \
+                    mets.structmap(type_attr='Fairdata-logical')
+
+                # Create logical structmap
+                categories = self.find_file_categories()
                 wrapper_div = mets.div(type_attr='logical', dmdid=dmdsec_id)
-                print "wrap %s " % ET.tostring(wrapper_div)
                 for category in categories:
-                    div_cat = mets.div(type_attr=category, dmdid=dmdsec_id)
-                    for file_ in categories.get(category):
-                        filename = file_
-                        fileid = get_fileid(filename, filesec_xml)
-                        div_cat.append(mets.fptr(fileid))
-                    wrapper_div.append(div_cat)
-                structmap.append(wrapper_div)
+                    div = mets.div(type_attr=category, dmdid=dmdsec_id)
+                    for filename in categories.get(category):
+                        fileid = self.get_fileid(filename)
+                        div.append(mets.fptr(fileid))
+                    wrapper_div.append(div)
+                logical_structmap.append(wrapper_div)
 
                 # Add the logical structmap into the same file where physical
                 # struct map already is
                 start = mets.structmap(type_attr='Fairdata-physical')
                 start.append(old_struct)
                 new_struct = ET.tostring(start)
-                new_struct += ET.tostring(structmap, pretty_print=True,
+                new_struct += ET.tostring(logical_structmap, pretty_print=True,
                                           xml_declaration=False,
                                           encoding='UTF-8')
-                print "new struct %s" %new_struct
 
                 with open(os.path.join(self.sip_creation_path,
                                        'structmap.xml'), 'r+') as struct_file:
@@ -153,18 +149,23 @@ class CreateStructMap(WorkflowTask):
         return locical_struct
 
 
-def get_fileid(filename, filesec_xml):
-    """get file id from filesec by filename"""
-    fileid = None
-    root = filesec_xml.getroot()
+    def get_fileid(self, filename):
+        """get file id from filesec by filename"""
 
-    files = root[0][0]
-    for file_ in files:
-        for file__ in file_:
-            if str(file__.get('{http://www.w3.org/1999/xlink}href'))[7:] \
-                    == filename:
-                print "file found %s " % file_.get('ID')
-                fileid = file_.get('ID')
-                break
+        # pylint: disable=no-member
+        filesec_xml = ET.parse(os.path.join(self.sip_creation_path,
+                                            'filesec.xml'))
 
-    return fileid
+        fileid = None
+        root = filesec_xml.getroot()
+
+        files = root[0][0]
+        for file_ in files:
+            for file__ in file_:
+                if str(file__.get('{http://www.w3.org/1999/xlink}href'))[7:] \
+                        == filename:
+                    print "file found %s " % file_.get('ID')
+                    fileid = file_.get('ID')
+                    break
+
+        return fileid
