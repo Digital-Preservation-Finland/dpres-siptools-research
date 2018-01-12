7f0b2996efd8126123d05c3eb0b059c2a0f0229f
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-01-12 14:13:06 +0200

    Enable more integration tests and add some documentation

--- tests/integration_tests/workflow_test.py
+++ tests/integration_tests/workflow_test.py
@@ -1,4 +1,10 @@
-"""Tests running parts of workflow"""
+"""Each workflow task should be able complete if it is directly called by luigi
+i.e. each task should know which other tasks are required to complete before it
+itself can be run. This module tests that each task will succesfully run all
+it's required tasks when it is called by luigi. Metax, Ida, and mongodb are
+mocked. Same sample dataset is used for testing all tasks. It is only tested
+that each task willl complete, the output of task is NOT examined.
+"""
 
 import os
 import pytest
@@ -11,9 +17,7 @@ def run_luigi_task(module, task, workspace):
     """Run luigi as it would be run from commandline"""
     with pytest.raises(SystemExit):
         luigi.cmdline.luigi_run(
-            ('--module',
-             module,
-             task,
+            ('--module', module, task,
              '--workspace', workspace,
              '--dataset-id', 'workflow_test_dataset_1',
              '--config', 'tests/data/siptools_research.conf',
@@ -35,16 +39,15 @@ def run_luigi_task(module, task, workspace):
         ('get_files', 'GetFiles'),
         ('create_techmd', 'CreateTechnicalMetadata'),
         ('create_structmap', 'CreateStructMap'),
+        ('create_mets', 'CreateMets'),
+        ('sign', 'SignSIP'),
+        ('compress', 'CompressSIP'),
+        ('send_sip', 'SendSIPToDP'),
         # These tests do not work yet:
-        # ('create_mets', 'CreateMets'),
-        # ('sign', 'SignSIP'),
-        # ('compress', 'CompressSIP'),
-        # ('send_sip', 'SendSIPToDP'),
         # ('report_preservation_status', 'ReportPreservationStatus'),
         # ('cleanup', 'CleanupWorkspace'),
     ]
 )
-
 @pytest.mark.timeout(200)
 def test_workflow(testpath, testmetax, testida, testmongoclient, module, task):
     """Run a task (and all tasks it requires) and check that check that report
