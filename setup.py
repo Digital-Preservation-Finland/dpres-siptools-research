bf6d52db79ea46dca9742830aa87cb3d7d564f03
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-02-09 17:25:21 +0200

    TPASPKT-14 Install workflow init command to /usr/bin

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ from setuptools import setup, find_packages
 
 def main():
     """Install siptools-research"""
-    setup(name='siptools_research',
-          packages=find_packages(exclude=['tests', 'tests.*']),
-          version='dev')
+    setup(
+        name='siptools_research',
+        packages=find_packages(exclude=['tests', 'tests.*']),
+        version='dev',
+        entry_points={
+            'console_scripts': [
+                'siptools_research = '\
+                'siptools_research.__main__.py:main'
+            ]
+        }
+    )
 
 if __name__ == '__main__':
     main()
