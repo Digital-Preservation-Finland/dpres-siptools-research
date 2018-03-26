6500d1d5a1f29652f47a855fee296ebb69f04780
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-03-26 18:09:55 +0300

    TPASPKT-88 Fix importing validate_dataset function

--- siptools_research/__init__.py
+++ siptools_research/__init__.py
@@ -1,6 +1,8 @@
 """Digital preservation packaging service for research datasets"""
-# Allow importing preserve_dataset straight from siptools_research module, for
-# example:
-
+# Allow importing preserve_dataset and validate_dataset functions straight from
+# siptools_research module, for example:
+#
 #    from siptools_research import preserve_dataset
+#    from siptools_research import validate_metadata
 from siptools_research.preserve_dataset import preserve_dataset
+from siptools_research.validate_metadata import validate_metadata
