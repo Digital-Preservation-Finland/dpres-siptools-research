15ec769402fea0207400c05cdd7a7652c9e11617
Esa Bister <esa.bister@csc.fi>
2018-05-18 12:53:42 +0000

    TPASPKT-99 Download header of the file in IDA

--- siptools_research/utils/ida.py
+++ siptools_research/utils/ida.py
@@ -1,6 +1,30 @@
 """IDA interface module"""
 import requests
 from siptools_research.config import Configuration
+import tempfile
+from subprocess import Popen, PIPE
+
+
+def _getResponse(identifier, config_file, stream=False):
+    conf = Configuration(config_file)
+    user = conf.get('ida_user')
+    password = conf.get('ida_password')
+    baseurl = conf.get('ida_url')
+    url = '%s/files/%s/download' % (baseurl, identifier)
+    try:
+        response = requests.get(url, auth=(user, password), verify=False,
+                                stream=stream)
+    except requests.exceptions.ConnectionError as exc:
+        raise Exception("Could not connect to Ida: %s" % exc.message)
+# Raise error if file is not found
+    if response.status_code == 404:
+        raise Exception("File not found in Ida.")
+    if response.status_code == 403:
+        raise Exception("Access to file forbidden.")
+    elif not response.status_code == 200:
+        raise Exception("File could not be retrieved.")
+    return response
+
 
 def download_file(identifier, filepath, config_file):
     """Download file from IDA. Ida url, username, and password are read from
@@ -12,24 +36,27 @@ def download_file(identifier, filepath, config_file):
     :config_file: Configuration file
     :returns: None
     """
-    conf = Configuration(config_file)
-    user = conf.get('ida_user')
-    password = conf.get('ida_password')
-    baseurl = conf.get('ida_url')
-
-    url = '%s/files/%s/download' % (baseurl, identifier)
-    try:
-        response = requests.get(url, auth=(user, password), verify=False)
-    except requests.exceptions.ConnectionError as exc:
-        raise Exception("Could not connect to Ida: %s" % exc.message)
-
-    # Raise error if file is not found
-    if response.status_code == 404:
-        raise Exception("File not found in Ida.")
-    if response.status_code == 403:
-        raise Exception("Access to file forbidden.")
-    elif not response.status_code == 200:
-        raise Exception("File could not be retrieved.")
+    response = _getResponse(identifier, config_file)
 
     with open(filepath, 'w') as new_file:
         new_file.write(response.content)
+
+
+def download_file_header(identifier, config_file):
+    """Downloads only the header of the file in IDA and creates a temporary
+    file the caller should delete if not needed anymore. Ida url, username,
+    and password are read from configuration file.
+
+    Function arguments:
+    :identifier: File identifier (for example "pid:urn:1")
+    :config_file: Configuration file
+    :returns: Path to the temp file containing the header of the file in IDA
+    """
+    response = _getResponse(identifier, config_file, stream=True)
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        for chunk in response.iter_content(chunk_size=512):
+            if chunk:  # filter out keep-alive new chunks
+                tmp.write(chunk)
+                break
+    return tmp.name
--- tests/unit_tests/utils/ida_test.py
+++ tests/unit_tests/utils/ida_test.py
@@ -17,3 +17,24 @@ def test_download_file(testpath):
     # The file should be a text file that says: "foo\n"
     with open(new_file_path, 'r') as new_file:
         assert new_file.read() == 'foo\n'
+
+
+@pytest.mark.usefixtures('testida')
+def test_download_big_file_header(testpath):
+    """Checks the size of the big file header created is 512 bytes.
+    """
+    header = ida.download_file_header('valid_tiff.tiff',
+                                      pytest.TEST_CONFIG_FILE)
+
+    assert os.path.getsize(header) == 512
+
+
+@pytest.mark.usefixtures('testida')
+def test_download_short_file_header(testpath):
+    """Checks the size of the short file header created is correct.
+    """
+    header = ida.download_file_header('pid:urn:1',
+                                      pytest.TEST_CONFIG_FILE)
+
+    # The file should be a text file that says: "foo\n"
+    assert os.path.getsize(header) == 4
