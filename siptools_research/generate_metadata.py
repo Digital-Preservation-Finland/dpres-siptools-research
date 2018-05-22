47275babcbfcaa4ac4aafbd5350408b6b002dfb3
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-05-22 17:33:37 +0300

    TPASPKT-95 Refactor generate_metadata function

--- siptools_research/generate_metadata.py
+++ siptools_research/generate_metadata.py
@@ -9,39 +9,50 @@ import siptools_research.utils.metax
 import siptools_research.utils.ida
 
 def generate_metadata(dataset_id, config="/etc/siptools_research.conf"):
-    """Generates technical metadata for all files of a given dataset and
-    updates relevant fields in Metax. If a field already has a value (set by
-    user) it will not be updated.
+    """Generates technical metadata and mix metadata for all files of a given
+    dataset and updates relevant fields in file metadata.
     """
     metax_client = siptools_research.utils.metax.Metax(config)
     for file_ in metax_client.get_dataset_files(dataset_id):
+
+        # Get file info
         file_id = file_['identifier']
+        file_metadata = metax_client.get_file(file_id)
 
         # Download file to tmp directory
         tmpfile = '/tmp/%s_%s' % (file_id, uuid.uuid4())
         siptools_research.utils.ida.download_file(file_id, tmpfile, config)
 
-        # Read technical metadata
-        metadata = siptools.scripts.import_object.metadata_info(tmpfile)
-
-        # Create file_characteristics
-        file_characteristics = {}
-        file_characteristics['file_format'] = metadata['format']['mimetype']
-        file_characteristics['format_version'] = metadata['format']['version']
-        if 'charset' in metadata['format'].keys():
-            file_characteristics['encoding'] = metadata['format']['charset']
-
-        # Merge generated file_characteristics with original data from Metax.
-        # If a field was already defined in original data, it will override the
-        # generated value.
-        file_characteristics.update(
-            metax_client.get_file(file_id)['file_characteristics']
-        )
-
-        # Update file_characteristics
+        # Generate and update file_characteristics
+        file_characteristics = _generate_techmd(tmpfile, file_metadata)
         metax_client.set_file_characteristics(file_id, file_characteristics)
 
         # Generate mix metadata
         if file_characteristics['file_format'].startswith('image'):
             mix_element = siptools.scripts.create_mix.write_mix(tmpfile)
             metax_client.set_xml(file_id, mix_element)
+
+def _generate_techmd(tmpfile, original_metadata):
+    """Reads file and generates technical metadata. Saves metada to
+    "file_characteristics" object in Metax. If a field already has a value
+    (set by user) it will not be updated.
+    """
+
+    # Generate technical metadata from file
+    metadata = siptools.scripts.import_object.metadata_info(tmpfile)
+
+    # Create file_characteristics object
+    file_characteristics = {}
+    file_characteristics['file_format'] = metadata['format']['mimetype']
+    file_characteristics['format_version'] = metadata['format']['version']
+    if 'charset' in metadata['format'].keys():
+        file_characteristics['encoding'] = metadata['format']['charset']
+
+    # Merge generated file_characteristics with original data from Metax.
+    # If a field was already defined in original data, it will override the
+    # generated value.
+    file_characteristics.update(
+        original_metadata['file_characteristics']
+    )
+
+    return file_characteristics
