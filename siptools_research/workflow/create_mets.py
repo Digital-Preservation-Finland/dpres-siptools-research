401467ec5db14d8f35049e578622882760ae13c0
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-10-05 13:24:07 +0300

    TPASPKT-143 Remove logging to workspace

--- siptools_research/workflow/create_mets.py
+++ siptools_research/workflow/create_mets.py
@@ -5,7 +5,6 @@ import os
 from luigi import LocalTarget
 from metax_access import Metax
 from siptools_research.config import Configuration
-from siptools_research.utils.contextmanager import redirect_stdout
 from siptools_research.workflowtask import WorkflowTask
 from siptools_research.workflowtask import InvalidMetadataError
 from siptools_research.workflow.create_structmap import CreateStructMap
@@ -39,33 +38,29 @@ class CreateMets(WorkflowTask):
         :returns: None
         """
 
-        # Redirect stdout to logfile
-        mets_log = os.path.join(self.workspace, "logs", 'create-mets.log')
-        with open(mets_log, 'w+') as log:
-            with redirect_stdout(log):
-                # Get contract id from Metax
-                config_object = Configuration(self.config)
-                metax_client = Metax(config_object.get('metax_url'),
-                                     config_object.get('metax_user'),
-                                     config_object.get('metax_password'))
-                metadata = metax_client.get_dataset(self.dataset_id)
-                contract_id = metadata["contract"]["id"]
-                if contract_id is None:
-                    raise InvalidMetadataError(
-                        'Dataset does not have contract id'
-                    )
-                if isinstance(contract_id, (int, long)):
-                    contract_id = str(contract_id)
-                metadata = metax_client.get_contract(contract_id)
-                contract_identifier = metadata["contract_json"]["identifier"]
-                contract_org_name \
-                    = metadata["contract_json"]["organization"]["name"]
+        # Get contract id from Metax
+        config_object = Configuration(self.config)
+        metax_client = Metax(config_object.get('metax_url'),
+                             config_object.get('metax_user'),
+                             config_object.get('metax_password'))
+        metadata = metax_client.get_dataset(self.dataset_id)
+        contract_id = metadata["contract"]["id"]
+        if contract_id is None:
+            raise InvalidMetadataError(
+                'Dataset does not have contract id'
+            )
+        if isinstance(contract_id, (int, long)):
+            contract_id = str(contract_id)
+        metadata = metax_client.get_contract(contract_id)
+        contract_identifier = metadata["contract_json"]["identifier"]
+        contract_org_name \
+            = metadata["contract_json"]["organization"]["name"]
 
-                # Compile METS
-                compile_mets.main(['tpas',
-                                   contract_org_name.encode('utf-8'),
-                                   contract_identifier,
-                                   '--workspace', self.sip_creation_path,
-                                   '--clean',
-                                   '--objid', self.dataset_id,
-                                   '--packagingservice', 'Packaging Service'])
+        # Compile METS
+        compile_mets.main(['tpas',
+                           contract_org_name.encode('utf-8'),
+                           contract_identifier,
+                           '--workspace', self.sip_creation_path,
+                           '--clean',
+                           '--objid', self.dataset_id,
+                           '--packagingservice', 'Packaging Service'])
