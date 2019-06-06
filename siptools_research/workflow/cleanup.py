2345e12ca0863c296fea85aede2ec5db28c427b2
Juho Kuisma <juho.kuisma@csc.fi>
2019-06-06 15:14:23 +0300

    TPASPKT-161 Removing also IDA files in the CleanupWorkspace task

--- siptools_research/workflow/cleanup.py
+++ siptools_research/workflow/cleanup.py
@@ -1,12 +1,16 @@
 """Luigi task that removes workspaces of finished workflows.
 """
-
 import os
 import shutil
+
+from metax_access import Metax
+
 from siptools_research.utils.database import Database
+from siptools_research.config import Configuration
 from siptools_research.workflowtask import WorkflowTask
-from siptools_research.workflow.report_preservation_status\
-    import ReportPreservationStatus
+from siptools_research.workflow.report_preservation_status import (
+    ReportPreservationStatus
+)
 
 
 class CleanupWorkspace(WorkflowTask):
@@ -15,6 +19,51 @@ class CleanupWorkspace(WorkflowTask):
     """
     success_message = 'Workspace was cleaned'
     failure_message = 'Cleaning workspace failed'
+
+    def ida_files_cleaned(self):
+        """Check if all the files are cleaned from
+        /var/spool/siptools_research/ida_files
+
+        :returns: Boolean
+        """
+        identifiers, ida_files_path = self.get_identifiers()
+
+        for identifier in identifiers:
+            filepath = os.path.join(ida_files_path, identifier)
+            if os.path.isfile(filepath):
+                return False
+
+        return True
+
+
+    def clean_ida_files(self):
+        """Remove identifiers from /var/spool/siptools_research/ida_files"""
+        identifiers, ida_files_path = self.get_identifiers()
+
+        for identifier in identifiers:
+            filepath = os.path.join(ida_files_path, identifier)
+            if os.path.isfile(filepath):
+                os.remove(filepath)
+
+    def get_identifiers(self):
+        """Return a list of all the file identifiers and the path to the
+        downloaded IDA files.
+
+        :returns: Tuple (list of identifiers, ida_files_path)
+        """
+        config_object = Configuration(self.config)
+        workspace_root = config_object.get("workspace_root")
+        ida_files_path = os.path.join(workspace_root, "ida_files")
+
+        metax_client = Metax(
+            config_object.get('metax_url'),
+            config_object.get('metax_user'),
+            config_object.get('metax_password'),
+            verify=config_object.getboolean('metax_ssl_verification')
+        )
+        dataset_files = metax_client.get_dataset_files(self.dataset_id)
+
+        return [_file["identifier"] for _file in dataset_files], ida_files_path
 
     def requires(self):
         """The Tasks that this Task depends on.
@@ -31,6 +80,8 @@ class CleanupWorkspace(WorkflowTask):
         :returns: None
         """
         shutil.rmtree(self.workspace)
+        self.clean_ida_files()
+
 
     def complete(self):
         """Task is complete when workspace does not exist, but
@@ -57,5 +108,5 @@ class CleanupWorkspace(WorkflowTask):
         if result != 'success':
             return False
 
-        # Check if workspace exists
-        return not os.path.exists(self.workspace)
+        # Check are the IDA files cleaned and if workspace exists
+        return self.ida_files_cleaned() and not os.path.exists(self.workspace)
