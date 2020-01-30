420e088060a0ddc539bc5fb59c4b491ac0210ff7
Esa Bister <ebister@csc.fi>
2020-01-30 14:08:54 +0200

    TPASPKT-442 Dataset preservation state set after file validation

--- siptools_research/file_validator.py
+++ siptools_research/file_validator.py
@@ -2,7 +2,9 @@
 import os
 
 from file_scraper.scraper import Scraper
-from metax_access.metax import Metax
+from metax_access.metax import (Metax, DS_STATE_INVALID_METADATA,
+                                DS_STATE_VALID_METADATA,
+                                DS_STATE_METADATA_VALIDATION_FAILED)
 
 from requests.exceptions import HTTPError
 
@@ -11,21 +13,17 @@ from siptools_research.utils.database import Database
 from siptools_research.utils.ida import download_file, IdaError
 
 
-def _download_files(dataset_id, config="/etc/siptools_research.conf"):
+def _download_files(metax_client, dataset_id,
+                    config_file="/etc/siptools_research.conf"):
     """Download all dataset files from IDA
 
+    :param metax_client: metax access
     :param dataset_id: dataset identifier
-    :param config: configuration file path
+    :param config_file: configuration file path
     :returns: A list of the metadata of all downloaded files
     """
-    conf = Configuration(config)
+    conf = Configuration(config_file)
     pas_storage_id = conf.get("pas_storage_id")
-    metax_client = Metax(
-        conf.get('metax_url'),
-        conf.get('metax_user'),
-        conf.get('metax_password'),
-        verify=conf.getboolean('metax_ssl_verification')
-    )
 
     dataset_files = metax_client.get_dataset_files(dataset_id)
     for dataset_file in dataset_files:
@@ -33,13 +31,11 @@ def _download_files(dataset_id, config="/etc/siptools_research.conf"):
         path = dataset_file["file_path"]
         if not dataset_file["file_storage"]["identifier"] == pas_storage_id:
             try:
-                download_file(identifier, config_file=config)
+                download_file(identifier, config_file=config_file)
             except (HTTPError, IdaError):
                 raise FileValidationError(
                     "Could not download file '%s' from IDA" % path, []
                 )
-
-
     return dataset_files
 
 
@@ -58,49 +54,88 @@ class FileValidationError(Exception):
         return message
 
 
-
-def validate_files(dataset_id, config="/etc/siptools_research.conf"):
+def validate_files(dataset_id, config_file="/etc/siptools_research.conf"):
     """Validate all files in a dataset.
 
     :param dataset_id: dataset identifier
     :param config: configuration file path
     :returns: ``True`` if all files are well-formed.
     """
-    conf = Configuration(config)
+    conf = Configuration(config_file)
     storage_id = conf.get("pas_storage_id")
-    mongo_files = Database(config).client.upload.files
+    metax_client = Metax(
+        conf.get('metax_url'),
+        conf.get('metax_user'),
+        conf.get('metax_password'),
+        verify=conf.getboolean('metax_ssl_verification')
+    )
+    mongo_files = Database(config_file).client.upload.files
     ida_path = os.path.join(conf.get("workspace_root"), "ida_files")
     errors = []
 
-    dataset_files = _download_files(dataset_id, config=config)
-    for dataset_file in dataset_files:
-        identifier = dataset_file["identifier"]
-        path = dataset_file["file_path"]
-        file_chars = dataset_file["file_characteristics"]
-        mimetype = file_chars["file_format"]
-        encoding = file_chars.get("encoding", None)
+    try:
+        # set default values
+        success = False
+        status_code = DS_STATE_METADATA_VALIDATION_FAILED
+        message = "Metadata validation failed"
+        dataset_files = _download_files(metax_client, dataset_id,
+                                        config_file=config_file)
+        for dataset_file in dataset_files:
+            mongo_file = mongo_files.find_one(
+                {"_id": dataset_file["identifier"]}
+            )
+            _validate_file(dataset_file, mongo_file, storage_id,
+                           ida_path, errors)
+        if errors:
+            raise FileValidationError(
+                "Following files are not well-formed:",
+                errors
+            )
+    except FileValidationError as exc:
+        status_code = DS_STATE_INVALID_METADATA
+        message = str(exc)
+        raise
+    else:
+        success = True
+        status_code = DS_STATE_VALID_METADATA
+        message = "Files passed validation"
+    finally:
+        message = message[:199] if len(message) > 200 else message
+        metax_client.set_preservation_state(dataset_id,
+                                            state=status_code,
+                                            system_description=message)
+    return success
 
-        if dataset_file["file_storage"]["identifier"] == storage_id:
-            mongo_file = mongo_files.find_one({"_id": identifier})
-            if not mongo_file:
-                raise FileValidationError(
-                    "File '%s' not found in pre-ingest file storage" % path, []
-                )
-            filepath = mongo_file["file_path"]
-        else:
-            filepath = os.path.join(ida_path, identifier)
 
-        scraper = Scraper(filepath, mimetype=mimetype, charset=encoding)
-        scraper.scrape(check_wellformed=True)
-        if not scraper.well_formed:
-            errors.append(path)
+def _validate_file(file_, mongo_file, storage_id, ida_path, errors):
+    """validates file using Scraper
 
-        del scraper
+    :param file_: file metadata
+    :param mongo_file: file data in mongo
+    :param storage_id: pas storage id
+    :param ida_path: ida path
+    :param errors: array to store non-valid files
+    :returns: None
+    """
+    identifier = file_["identifier"]
+    path = file_["file_path"]
+    file_chars = file_["file_characteristics"]
+    mimetype = file_chars["file_format"]
+    encoding = file_chars.get("encoding", None)
 
-    if errors:
-        raise FileValidationError(
-            "Following files are not well-formed:",
-            errors
-        )
+    if file_["file_storage"]["identifier"] == storage_id:
+        if not mongo_file:
+            raise FileValidationError(
+                "File '%s' not found in pre-ingest file storage" %
+                path, []
+            )
+        filepath = mongo_file["file_path"]
+    else:
+        filepath = os.path.join(ida_path, identifier)
 
-    return True
+    scraper = Scraper(filepath, mimetype=mimetype, charset=encoding)
+    scraper.scrape(check_wellformed=True)
+    if not scraper.well_formed:
+        errors.append(path)
+
+    del scraper
