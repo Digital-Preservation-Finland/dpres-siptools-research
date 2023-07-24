06f197404fbf4bfe4be2f0676ff6f7a79c13ef15
Marianna Ojanen <ojanenma@csc.fi>
2023-07-24 09:48:53 +0000

    TPASPKT-991 Add support for both mongoengine versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -56,7 +56,8 @@ def mock_upload_conf(monkeypatch):
     if mongoengine.__version__ <= "0.24.2":
         mongoengine.connect(host="mongomock://localhost/upload", tz_aware=True)
     else:
-        mongoengine.connect("upload", host="mongodb://localhost", tz_aware=True,
+        mongoengine.connect("upload", host="mongodb://localhost",
+                            tz_aware=True,
                             mongo_client_class=mongomock.MongoClient)
 
 
