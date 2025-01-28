ec5f19b1a2b1171dbef6ae5753722691952901d9
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2025-01-28 16:58:50 +0200

    TPASPKT-1292 Metax API V3 tests for ValidateMetadata task

--- tests/unit_tests/workflow/validate_metadata_test.py
+++ tests/unit_tests/workflow/validate_metadata_test.py
@@ -4,11 +4,13 @@ import copy
 
 import pytest
 
+import tests.metax_data.datasetsV3
+import tests.metax_data.filesV3
 from siptools_research.exceptions import InvalidDatasetMetadataError
 from siptools_research.workflow.validate_metadata import ValidateMetadata
 from tests.metax_data.datasets import BASE_DATASET
 from tests.metax_data.files import TXT_FILE
-from tests.utils import add_metax_v2_dataset
+from tests.utils import add_metax_dataset, add_metax_v2_dataset
 
 
 @pytest.mark.usefixtures("testmongoclient")
@@ -21,7 +23,23 @@ def test_validatemetadata(config, workspace, requests_mock):
     :param workspace: Temporary directory fixture
     :param requests_mock: Mocker object
     """
-    # Mock metax. Create a valid dataset that contains two text files.
+    # Mock Metax API V3. Create a valid dataset that contains two text
+    # files.
+    file1 = copy.deepcopy(tests.metax_data.filesV3.TXT_FILE)
+    file1["id"] = "identifier1"
+    file1["pathname"] = "/path1"
+    file2 = copy.deepcopy(tests.metax_data.filesV3.TXT_FILE)
+    file2["id"] = "identifier2"
+    file2["pathname"] = "/"
+    dataset = copy.deepcopy(tests.metax_data.datasetsV3.BASE_DATASET)
+    dataset["id"] = workspace.name
+    add_metax_dataset(
+        requests_mock=requests_mock,
+        dataset=dataset,
+        files=[file1, file2]
+    )
+
+    # Mock metax API V2
     file1 = copy.deepcopy(TXT_FILE)
     file1['identifier'] = 'identifier1'
     file1['file_path'] = '/path1'
@@ -57,8 +75,14 @@ def test_invalid_metadata(config, workspace, requests_mock):
     :param workspace: Temporary workspace directory fixture
     :param requests_mock: HTTP request mocker
     """
-    # Mock Metax. Remove "contract" from dataset metadata to create an
+    # Mock Metax API V3. Remove "contract" from dataset metadata to create an
     # invalid dataset.
+    dataset = copy.deepcopy(tests.metax_data.datasetsV3.BASE_DATASET)
+    dataset["id"] = workspace.name
+    dataset["preservation"]["contract"] = None
+    requests_mock.get(f"/v3/datasets/{workspace.name}", json=dataset)
+
+    # Mock Metax API V2
     dataset = copy.deepcopy(BASE_DATASET)
     dataset['identifier'] = workspace.name
     del dataset['contract']
