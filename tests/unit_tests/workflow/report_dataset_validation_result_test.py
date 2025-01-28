e9e6bd44dac0b895755e05fddd372666eac16192
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2025-01-28 16:10:16 +0200

    TPASPKT-1293 Metax API V3 tests for ReportDatasetValidationResult task

--- tests/unit_tests/workflow/report_dataset_validation_result_test.py
+++ tests/unit_tests/workflow/report_dataset_validation_result_test.py
@@ -1,20 +1,29 @@
 """Unit tests for ReportDatasetValidationResult task."""
-
 import pytest
 from metax_access.metax import DS_STATE_METADATA_CONFIRMED
 
+import tests.metax_data
 from siptools_research.workflow import report_dataset_validation_result
 
 
 @pytest.mark.usefixtures('testmongoclient')
-def test_reportdatasetvalidationresult(config, workspace, requests_mock):
+def test_reportdatasetvalidationresult(config, workspace, requests_mock,
+                                       request):
     """Test reporting dataset validation result.
 
     :param config: Configuration file
     :param workspace: Temporary workspace directory
     :param requests_mock: HTTP request mocker
+    :param request: Pytest CLI arguments
     """
-    # Mock Metax
+    # Mock Metax API V3
+    dataset = tests.metax_data.datasetsV3.BASE_DATASET
+    dataset["id"] = workspace.name
+    requests_mock.get(f"/v3/datasets/{workspace.name}", json=dataset)
+    patch_dataset \
+        = requests_mock.patch(f"/v3/datasets/{workspace.name}/preservation")
+
+    # Mock Metax API V2
     requests_mock.get(
         f'/rest/v2/datasets/{workspace.name}',
         json={'identifier': 'foo',
@@ -29,7 +38,7 @@ def test_reportdatasetvalidationresult(config, workspace, requests_mock):
                 }
             }
     )
-    patch_dataset = requests_mock.patch('/rest/v2/datasets/foo')
+    v2_patch_dataset = requests_mock.patch('/rest/v2/datasets/foo')
 
     # Init and run task
     task = report_dataset_validation_result.ReportDatasetValidationResult(
@@ -41,8 +50,14 @@ def test_reportdatasetvalidationresult(config, workspace, requests_mock):
     assert task.complete()
 
     # The dataset preservation state should be updated
-    assert patch_dataset.called_once
-    assert patch_dataset.last_request.json() \
-        == {'preservation_state': DS_STATE_METADATA_CONFIRMED,
-            'preservation_description':
-            'Metadata and files validated'}
+    if request.config.getoption("--v3"):
+        assert patch_dataset.called_once
+        assert patch_dataset.last_request.json() \
+            == {"state": DS_STATE_METADATA_CONFIRMED,
+                "description": {"en": "Metadata and files validated"}}
+    else:
+        assert v2_patch_dataset.called_once
+        assert v2_patch_dataset.last_request.json() \
+            == {'preservation_state': DS_STATE_METADATA_CONFIRMED,
+                'preservation_description':
+                'Metadata and files validated'}
