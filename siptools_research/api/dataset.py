496d541ff35e3840516369a2f79152ef5f1c6374
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2025-10-09 17:06:04 +0300

    TPASPKT-998 Move dataset blueprint to separate module

---
+++ siptools_research/api/dataset.py
@@ -0,0 +1,81 @@
+"""API for managing datasets."""
+from flask import Blueprint, current_app, jsonify, request
+
+from siptools_research.dataset import Dataset
+
+dataset = Blueprint("dataset", "dataset")
+
+
+@dataset.route('/<dataset_id>/validate', methods=['POST'])
+def validate_dataset(dataset_id):
+    """Validate dataset metadata and files.
+
+    :returns: HTTP Response
+    """
+    Dataset(identifier=dataset_id,
+            config=current_app.config.get("SIPTOOLS_RESEARCH_CONF")).validate()
+
+    response = jsonify({'dataset_id': dataset_id,
+                        'status': 'validating dataset'})
+    response.status_code = 202
+
+    return response
+
+
+@dataset.route('/<dataset_id>/preserve', methods=['POST'])
+def preserve(dataset_id):
+    """Trigger preservation workflow for dataset.
+
+    :returns: HTTP Response
+    """
+    Dataset(identifier=dataset_id,
+            config=current_app.config.get("SIPTOOLS_RESEARCH_CONF")).preserve()
+
+    response = jsonify({'dataset_id': dataset_id,
+                        'status': 'preserving'})
+    response.status_code = 202
+
+    return response
+
+
+@dataset.route('/<dataset_id>/generate-metadata', methods=['POST'])
+def generate_metadata(dataset_id):
+    """Generate technical metadata and store it to Metax.
+
+    :returns: HTTP Response
+    """
+    Dataset(
+        identifier=dataset_id,
+        config=current_app.config.get("SIPTOOLS_RESEARCH_CONF")
+    ).generate_metadata()
+
+    response = jsonify({'dataset_id': dataset_id,
+                        'status': 'generating metadata'})
+    response.status_code = 202
+
+    return response
+
+
+@dataset.route('/<dataset_id>/reset', methods=['POST'])
+def reset(dataset_id: str):
+    """Reset dataset.
+
+    :returns: HTTP response
+    """
+    description = request.form["description"]
+    reason_description = request.form["reason_description"]
+
+    Dataset(
+        identifier=dataset_id,
+        config=current_app.config.get("SIPTOOLS_RESEARCH_CONF")
+    ).reset(
+        description=description,
+        reason_description=reason_description
+    )
+
+    response = jsonify({
+        'dataset_id': dataset_id,
+        'status': 'dataset has been reset'
+    })
+
+    return response
--- tests/app_test.py
+++ tests/app_test.py
@@ -3,9 +3,7 @@ import flask
 import pytest
 from metax_access import ResourceNotAvailableError
 
-from siptools_research.dataset import Dataset
 from siptools_research.models.file_error import FileError
-import tests.utils
 
 
 @pytest.fixture()
@@ -48,87 +46,6 @@ def test_index(client):
     response = client.get("/")
 
     assert response.status_code == 400
-
-
-def test_dataset_preserve(client, config):
-    """Test preserving dataset.
-
-    :param client: Flask test client
-    :param config: Configuration file
-    """
-    response = client.post("/dataset/1/preserve")
-    assert response.status_code == 202
-    assert response.json == {
-        "dataset_id": "1",
-        "status": "preserving"
-    }
-
-    dataset = Dataset("1", config=config)
-    assert dataset.enabled is True
-    assert dataset.target.value == "preservation"
-
-
-def test_dataset_generate_metadata(client, config):
-    """Test the generating metadata.
-
-    :param client: Flask test client
-    :param config: Configuration file
-    """
-    response = client.post("/dataset/1/generate-metadata")
-    assert response.status_code == 202
-    assert response.json == {
-        "dataset_id": "1",
-        "status": "generating metadata"
-    }
-
-    dataset = Dataset("1", config=config)
-    assert dataset.enabled is True
-    assert dataset.target.value == "metadata_generation"
-
-
-def test_dataset_reset(client, requests_mock):
-    """Test resetting the dataset.
-
-    :param client: Flask test client
-    :param requests_mock: HTTP Request mocker
-    """
-    # Mock Metax
-    tests.utils.add_metax_dataset(requests_mock)
-    patch_many = requests_mock.post("/v3/files/patch-many")
-
-    response = client.post(
-        "/dataset/test_dataset_id/reset",
-        data={
-            "description": "Reset by user",
-            "reason_description": "File was incorrect"
-        }
-    )
-    assert response.status_code == 200
-    assert response.json == {
-        "dataset_id": "test_dataset_id",
-        "status": "dataset has been reset"
-    }
-
-    # Dataset does not contain any files, so empty list of files should
-    # be patched
-    assert patch_many.called_once
-    assert patch_many.last_request.json() == []
-
-
-
-def test_validate_dataset(client, config):
-    """Test validating files and metadata.
-
-    :param client: Flask test client
-    :param config: Configuration file
-    """
-    response = client.post("/dataset/1/validate")
-    assert response.status_code == 202
-    assert response.json == {"dataset_id": "1", "status": "validating dataset"}
-
-    dataset = Dataset("1", config=config)
-    assert dataset.enabled
-    assert dataset.target.value == "validation"
 
 
 @pytest.mark.parametrize(
