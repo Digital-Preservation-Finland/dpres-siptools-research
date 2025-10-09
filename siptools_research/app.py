d4b3261a70db24240e226104d1e3e7b1acd8dc81
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2025-10-09 17:15:26 +0300

    TPASPKT-998 Move /file-errors API to Blueprint

--- siptools_research/app.py
+++ siptools_research/app.py
@@ -2,7 +2,7 @@
 import logging
 import os
 
-from flask import Flask, abort, current_app, jsonify, request
+from flask import Flask, abort, current_app, jsonify, request, Blueprint
 from metax_access import ResourceNotAvailableError
 
 import siptools_research.api.dataset
@@ -28,55 +28,9 @@ def create_app():
                                                      SIPTOOLS_RESEARCH_CONF)
     app.register_blueprint(siptools_research.api.dataset.dataset,
                            url_prefix="/dataset")
+    app.register_blueprint(file_errors)
 
     connect_mongoengine(app.config['SIPTOOLS_RESEARCH_CONF'])
-
-    @app.route(
-        '/file-errors',
-        # 'GET' only accepts 8 KB of query parameters, which would limit
-        # the amount of entries we can retrieve at a time.
-        # TODO: Maybe use QUERY HTTP method here if/once that becomes
-        # possible?
-        methods=['POST']
-    )
-    def get_file_errors():
-        """Return file errors for the given files.
-
-        Following parameters can be given to filter the results:
-        * ids = comma-separated list of file ID
-        * storage_service = storage service of the file
-        * storage_identifiers = comma-separated list of file storage
-          identifiers
-        * dataset_id = optional dataset identifier
-        """
-        filter_kwargs = {}
-
-        if ids := request.form.get("ids"):
-            filter_kwargs["file_id__in"] = ids.split(",")
-
-        if storage_service := request.form.get("storage_service"):
-            filter_kwargs["storage_service"] = storage_service
-
-        if storage_identifiers := request.form.get("storage_identifiers"):
-            filter_kwargs["storage_identifier__in"] = \
-                storage_identifiers.split(",")
-
-        if dataset_id := request.form.get("dataset_id"):
-            filter_kwargs["dataset_id"] = dataset_id
-
-        file_errors = FileError.objects.filter(**filter_kwargs)
-
-        return jsonify([
-            {
-                "id": error.file_id,
-                "storage_identifier": error.storage_identifier,
-                "storage_service": error.storage_service.value,
-                "dataset_id": error.dataset_id,
-                "errors": error.errors,
-                "created_at": error.created_at.isoformat()
-            }
-            for error in file_errors
-        ])
 
     @app.route('/')
     def index():
@@ -126,3 +80,55 @@ def create_app():
         return response
 
     return app
+
+
+file_errors = Blueprint("file_errors", "file_errors")
+
+
+@file_errors.route(
+    '/file-errors',
+    # 'GET' only accepts 8 KB of query parameters, which would limit
+    # the amount of entries we can retrieve at a time.
+    # TODO: Maybe use QUERY HTTP method here if/once that becomes
+    # possible?
+    methods=['POST']
+)
+def get_file_errors():
+    """Return file errors for the given files.
+
+    Following parameters can be given to filter the results:
+    * ids = comma-separated list of file ID
+    * storage_service = storage service of the file
+    * storage_identifiers = comma-separated list of file storage
+      identifiers
+    * dataset_id = optional dataset identifier
+    """
+    filter_kwargs = {}
+
+    if ids := request.form.get("ids"):
+        filter_kwargs["file_id__in"] = ids.split(",")
+
+    if storage_service := request.form.get("storage_service"):
+        filter_kwargs["storage_service"] = storage_service
+
+    if storage_identifiers := request.form.get("storage_identifiers"):
+        filter_kwargs["storage_identifier__in"] = \
+            storage_identifiers.split(",")
+
+    if dataset_id := request.form.get("dataset_id"):
+        filter_kwargs["dataset_id"] = dataset_id
+
+    file_errors = FileError.objects.filter(**filter_kwargs)
+
+    return jsonify([
+        {
+            "id": error.file_id,
+            "storage_identifier": error.storage_identifier,
+            "storage_service": error.storage_service.value,
+            "dataset_id": error.dataset_id,
+            "errors": error.errors,
+            "created_at": error.created_at.isoformat()
+        }
+        for error in file_errors
+    ])
+
